2773d370c4d6bef1f8bc6c0821a3d581b83bf62f
JingGu101 <91243751+JingGu101@users.noreply.github.com>
2022-02-03 14:50:21 -0800

    Create TestByTeam.java

---
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/imports/TestByTeam.java
@@ -0,0 +1,47 @@
+package com.puppycrawl.tools.checkstyle.checks.imports;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+import static com.puppycrawl.tools.checkstyle.checks.imports.AvoidStarImportCheck.MSG_KEY;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class TestByTeam
+        extends AbstractModuleTestSupport {
+
+    @Override
+    protected String getPackageLocation() {
+        return "com/puppycrawl/tools/checkstyle/checks/imports/avoidstarimport";
+    }
+
+
+    @Test
+    public void testDefaultOperationTest()
+            throws Exception {
+        final String[] expected = {
+                "12:54: " + getCheckMessage(MSG_KEY, "com.puppycrawl."
+                        + "tools.checkstyle.checks.imports.*"),
+                "14:15: " + getCheckMessage(MSG_KEY, "java.io.*"),
+                "15:17: " + getCheckMessage(MSG_KEY, "java.lang.*"),
+
+        };
+
+        verifyWithInlineConfigParser(
+                getPath("hasViolationStar.java"),
+                expected);
+    }
+
+    @Test
+    public void testDefaultOperationTestAllvalid()
+            throws Exception {
+        final String[] expected = {
+        };
+
+        verifyWithInlineConfigParser(
+                getPath("NoViolationStar.java"),
+                expected);
+    }
+    
+}
