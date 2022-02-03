2adcfa798b1a2f977596952505c3a3509bc44ac7
JingGu101 <91243751+JingGu101@users.noreply.github.com>
2022-02-03 14:44:27 -0800

    Create NoViolationStar.java

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/avoidstarimport/NoViolationStar.java
@@ -0,0 +1,56 @@
+/*
+AvoidStarImport
+excludes = (default)
+allowClassImports = (default)false
+allowStaticMemberImports = (default)false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.imports.avoidstarimport;
+
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.List;
+import java.lang.AbstractMethodError;
+import java.util.Iterator;
+import java.util.Enumeration;
+import java.util.Arrays;
+
+import javax.swing.JToolBar;
+import javax.swing.JToggleButton;
+import javax.swing.ScrollPaneLayout;
+import javax.swing.BorderFactory;
+import static java.io.File.listRoots;
+
+
+
+import java.awt.Component;
+import java.awt.Graphics2D;
+import java.awt.HeadlessException;
+import java.awt.Label;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.BitSet;
+
+import com.puppycrawl.tools.checkstyle.Checker;
+import com.puppycrawl.tools.checkstyle.CheckerTest;
+import com.puppycrawl.tools.checkstyle.Definitions;
+import com.puppycrawl.tools.checkstyle.ConfigurationLoaderTest;
+import com.puppycrawl.tools.checkstyle.PackageNamesLoader;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.DefaultLogger;
+
+/**
+ * Test case for imports
+ * Here's an import used only by javadoc: {@link Date}.
+ * @author Oliver Burn
+ * @author lkuehne
+ * @author Michael Studman
+ * @see Calendar Should avoid unused import for Calendar
+ **/
+class NoViolationStar
+{
+
+}
